d53319a38053610c05a8081713a23509ef297704
Abylay <a.toremuratuly@mail.ru>
2025-03-19 11:25:45 +0500

    practice work number 4

---
+++ practice_work4/backend/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter 
+from .views import ItemViewSet, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
+ 
+router = DefaultRouter() 
+router.register(r'items', ItemViewSet) 
+
+urlpatterns = [ 
+    path('', include(router.urls)), 
+]
+ 
+urlpatterns += [ 
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+] 
+
+urlpatterns += [ 
+    path('register/', RegisterView.as_view(), name='register'), 
+] 
---
+++ practice_work4/frontend/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Login</h2>
+      <form @submit.prevent="handleLogin">
+        <input v-model="username" placeholder="Username" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { mapActions } from 'vuex';
+  
+  export default {
+    data() {
+      return { username: '', password: '' };
+    },
+    methods: {
+      ...mapActions(['login']),
+      async handleLogin() {
+        try {
+          await this.login({ username: this.username, password: this.password });
+          this.$router.push('/admin');
+        } catch (error) {
+          console.error('Login error:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
